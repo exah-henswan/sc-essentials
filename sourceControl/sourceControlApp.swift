2d76aa14672df6a0187197c11e1c368e8ab75df3
Hendrik Swanepoel <hennie@exah.co.za>
2022-11-01 15:35:23 +0200

    Initial Commit

---
+++ sourceControl/sourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  sourceControlApp.swift
+//  sourceControl
+//
+//  Created by Hendrik Gerrit Swanepoel on 2022/11/01.
+//
+
+import SwiftUI
+
+@main
+struct sourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
